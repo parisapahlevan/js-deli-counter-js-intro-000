7aea27c3c26928c89ba1072dc3fd8f57aee523d4
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-28 17:41:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function takeANumber(line, name){
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+  }
+  
+  function nowServing(katzDeliLine){
+    if (katzDeliLine.length === 0){
+      return "There is nobody waiting to be served!"
+    } else {
+      return `Currently serving ${katzDeliLine.shift()}.`
+    }	
+  }
+  
+  
+  
+  
+
+  
+    
+   
+   
